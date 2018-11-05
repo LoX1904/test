f925172785fc55c9fb108a292a2628d4d791c8fc
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2018-11-05 10:49:05 +0100

    Update test.java

--- test.java
+++ test.java
@@ -1,8 +1,9 @@
 package test;
-
+import test.func;
 
 public class test{  
   public static void main(String[] args){
     System.out.println("Hello");
+    System.out.println(func.add(3, 2));
   }
 }
