557c8794749276f123093ded5fbdbcc563cd1247
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2018-11-05 15:06:41 +0100

    Update Main.java

--- test/Main.java
+++ test/Main.java
@@ -1,6 +1,7 @@
 package test;
 
 import test.fu.Func;
+import test.fu.ck.Bla;
 
 public class Main{  
   public static void main(String[] args){
@@ -8,5 +9,6 @@ public class Main{
     System.out.println("Hello");
     System.out.println(f.add(3, 2));
     f.sayHI();
+    Bla.ausgeben("FIC");
   }
 }
