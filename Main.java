ccab0da7ea84e80e7c88896ec3e97f8de20633bd
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2018-11-05 11:27:04 +0100

    Update and rename Test.java to Main.java

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 package test;
 import test.Func;
 
-public class Test{  
+public class Main{  
   public static void main(String[] args){
     System.out.println("Hello");
     System.out.println(Func.add(3, 2));
