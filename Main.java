b15eb0adacbce9820c0fe4b3ae0c9066714f95ce
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2018-11-05 11:33:49 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
-//package test;
-//import test.Func;
+package test;
+import test.Func;
 
 public class Main{  
   public static void main(String[] args){
     System.out.println("Hello");
-   // System.out.println(Func.add(3, 2));
+    System.out.println(Func.add(3, 2));
   }
 }
