51789c33f5c5adc4cabf08a0f63618a06a006b51
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2018-11-21 14:58:08 +0100

    Update Bla.java

--- test/fu/ck/Bla.java
+++ test/fu/ck/Bla.java
@@ -2,6 +2,10 @@ package test.fu.ck;
 
 public class Bla{
   
+  private Bla(){
+    throw new IllegalStateException("Utility Class");
+  }
+  
   public static void ausgeben(String x){
     System.out.println(x);
   }
