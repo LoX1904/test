ba4f4efb7620a82f315d46165ba0e6bc1652303e
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2020-06-20 11:33:35 +0200

    Update Main.java

--- test/Main.java
+++ test/Main.java
@@ -2,12 +2,15 @@ package test;
 
 import test.fu.Func;
 import test.fu.ck.Bla;
+import java.util.Scanner;
 
 public class Main{  
   public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int value = s.nextInt();
     Func f = new Func();
     System.out.println("Hello");
-    System.out.println(f.add(3, 2));
+    System.out.println(f.add(i, 2));
     f.sayHI();
     Bla.ausgeben("FIC");
   }
