be7995fd62922f8f1ac1e5e540b1c90a5ce4fa4e
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2020-06-20 11:47:34 +0200

    Update Main.java

--- test/Main.java
+++ test/Main.java
@@ -3,9 +3,11 @@ package test;
 import test.fu.Func;
 import test.fu.Bla;
 import java.util.Scanner;
-
+import java.io.*;
 public class Main{  
   public static void main(String[] args){
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    
     Scanner s = new Scanner(System.in);
     int value = s.nextInt();
     
