afd1f336d4285e41cd02efcaf60e9f67b7fa3fc9
LoX1904 <33057480+LoX1904@users.noreply.github.com>
2020-06-20 11:44:37 +0200

    Update Main.java

--- test/Main.java
+++ test/Main.java
@@ -8,10 +8,12 @@ public class Main{
   public static void main(String[] args){
     Scanner s = new Scanner(System.in);
     int value = s.nextInt();
+    
     Func f = new Func();
-    System.out.println("Hello");
     System.out.println(f.add(value, 2));
     f.sayHI();
-    Bla.ausgeben("FIC");
+    
+    System.out.println("Hello");
+    Bla.ausgeben("Test");
   }
 }
